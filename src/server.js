5d222894e1f715f13381228371ffc9ac1abb5808
Sundeep Bhaskar Tonse <stonse@stonse-m02.ad.savagebeast.com>
2023-08-14 15:34:30 -0700

    Init

---
+++ src/server.js
@@ -0,0 +1,11 @@
+//Node JS Simple App
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+     res.send("Welcome to my awesome app!"); 
+ });
+
+app.listen(3000, function () {
+    console.log("app listening on port 3000");
+});
